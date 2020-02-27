2c8c5f2c48429ddc5e0bca1c25048cd7820e4df9
Jason Coltier <jasoncoltier@gmail.com>
2020-02-27 12:51:26 +0100

    Push UI

---
+++ Lumoa/Assets/Scripts/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Récupération du joueur à déterminer sur Unity
+    public Transform targetPlayer;
+
+    //Récupération du boss à déterminer sur Unity
+    public Transform targetBoss;
+
+    //"Vitesse" de déplacement de la caméra [0, 1]
+    public float smoothSpeed;
+    
+    //Position de base de la caméra
+    public Vector3 offset;
+
+    public float border;
+
+    private Vector3 cameraPos;
+    private Vector3 smoothedCameraPos;
+
+    private float distance;
+
+    private void FixedUpdate()
+    {
+        distance = Vector3.Distance(targetPlayer.position, new Vector3(0, 0, 0));
+        
+        if (distance > border)
+        {
+            /*
+            radian = Mathf.Atan2(targetPlayer.position.z - targetBoss.position.z, targetPlayer.position.x - targetBoss.position.x);
+            print(Mathf.Cos(radian));
+            print(Mathf.Sin(radian));
+            cameraPos = offset + Vector3.up * (distance - border) + Vector3.Scale(Vector3.up, Vector3.Lerp(targetBoss.position, targetPlayer.position, Mathf.Sin(radian) < 0 ? Mathf.Sin(radian) : Mathf.Sin(radian) * -1));
+            */
+            cameraPos = offset + Vector3.back * offset.z * distance / border * -1 + Vector3.up * (distance - border) + Vector3.Lerp(targetBoss.position, targetPlayer.position, 0.5f);
+        }
+        else
+        {
+            cameraPos = offset + Vector3.back * offset.z * distance / border * -1 + Vector3.Lerp(targetBoss.position, targetPlayer.position, 0.5f);
+        }
+
+        smoothedCameraPos = Vector3.Lerp(transform.position, cameraPos, smoothSpeed);
+        transform.position = smoothedCameraPos;
+
+    }
+
+}
---
+++ Lumoa/Assets/Scripts/PlayerLifeScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLifeScript : LifeScript
+{
+    public Slider LifeBar;
+    private bool isPlayerDead = false;
+    public Canvas canvas;
+
+    public override void Death()
+    {
+        Debug.Log("DEATH");
+        isPlayerDead = true;
+//        canvas.enabled = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationX;
+        End();
+    }
+
+    public override void End()
+    {
+        Menu.IsGameEnded = true;
+    }
+
+    public override void UpdateLife(int nb)
+    {
+        base.UpdateLife(nb);
+//        LifeBar.value = (float) CurrentLife / MaxLife;
+
+    }
+
+    public bool IsPlayerDead
+    {
+        get { return isPlayerDead; }
+        set { isPlayerDead = value; }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            UpdateLife(-1);
+        }
+    }
+}
+    
